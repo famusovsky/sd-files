944deb03220a71921edd663893b9be7858440081
Alexey Stepanov <st3p-by-step@ya.ru>
2022-12-28 15:49:41 +0300

    First version of directory scanning is implemented.

--- src/DirectoryScanner.java
+++ src/DirectoryScanner.java
@@ -1,3 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class DirectoryScanner {
-    //TODO: implement directory scanning
+    File directory;
+
+    public DirectoryScanner(String path) throws Exception {
+        directory = new File(path);
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException("Path is not a directory");
+        }
+    }
+
+    public ArrayList<FileCoin> getFileCoins() {
+        ArrayList<FileCoin> fileCoins = new ArrayList<>();
+        try {
+            scanDirectory(directory, fileCoins);
+        } catch (FileNotFoundException e) {
+            System.out.println("Произошла ошибка при чтении файла");
+        } catch (SecurityException e) {
+            System.out.println("Произошла ошибка доступа к файлу, из-за этого некоторые файлы могли быть не прочитаны");
+        }
+        return fileCoins;
+    }
+
+    public String getDirectoryPath() {
+        return directory.getAbsolutePath();
+    }
+
+    private void scanDirectory(File directory, ArrayList<FileCoin> fileCoins) throws FileNotFoundException, SecurityException {
+        try {
+            for (File item : Objects.requireNonNull(directory.listFiles())) {
+                if (item.isDirectory()) {
+                    scanDirectory(item, fileCoins);
+                } else {
+                    if (!item.getName().endsWith(".txt")) {
+                        continue;
+                    }
+                    try (FileReader fileReader = new FileReader(item)) {
+                        StringBuilder fileText = new StringBuilder();
+                        int ch;
+                        while ((ch = fileReader.read()) != -1) {
+                            fileText.append((char) ch);
+                        }
+                        fileCoins.add(new FileCoin(item.getAbsolutePath(), fileText.toString()));
+                    } catch (IOException e) {
+                        throw new FileNotFoundException();
+                    }
+                }
+            }
+        } catch (NullPointerException e) {
+            throw new FileNotFoundException();
+        } catch (SecurityException e) {
+            throw new SecurityException();
+        }
+    }
 }
